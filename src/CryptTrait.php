6bd6ecf66a08bd507498887da1d0c62102162b77
Eric Tendian <erictendian@gmail.com>
2018-09-11 12:56:19 -0500

    Adding alternative encryption/decryption using libsodium secretbox to CryptTrait

--- src/CryptTrait.php
+++ src/CryptTrait.php
@@ -32,6 +32,10 @@ trait CryptTrait
     protected function encrypt($unencryptedData)
     {
         try {
+            if (extension_loaded('sodium')) {
+                $nonce = \random_bytes(SODIUM_CRYPTO_SECRETBOX_NONCEBYTES);
+                return \sodium_bin2hex(\sodium_crypto_secretbox($unencryptedData, $nonce, $this->encryptionKey)).'.'.\sodium_bin2hex($nonce);
+            }
             return Crypto::encryptWithPassword($unencryptedData, $this->encryptionKey);
         } catch (\Exception $e) {
             throw new \LogicException($e->getMessage());
@@ -50,6 +54,12 @@ trait CryptTrait
     protected function decrypt($encryptedData)
     {
         try {
+            if (extension_loaded('sodium')) {
+                $encDataWithNonce = explode('.', $encryptedData);
+                $encryptedData = \sodium_hex2bin($encDataWithNonce[0]);
+                $nonce = \sodium_hex2bin($encDataWithNonce[1]);
+                return \sodium_crypto_secretbox_open($encryptedData, $nonce, $this->encryptionKey);
+            }
             return Crypto::decryptWithPassword($encryptedData, $this->encryptionKey);
         } catch (\Exception $e) {
             throw new \LogicException($e->getMessage());
